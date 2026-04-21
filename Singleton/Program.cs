cab0dbff5f9d9a2182ded738d9a941f55ac90026
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-04-21 10:51:06 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Singleton;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Singleton;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+
+    }
+    public static Singleton CreateInstance()
+    {
+        if (instance == null)
+        {
+            instance= new Singleton();
+        }
+        return instance;
+    }
+}
