4adafb6f63b6627fec8596f41488d3535c7a7723
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-05-23 16:51:51 +0300

    Naive Singlton, not thread safe

---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Singleton_Pattern;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Singleton_Pattern/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Pattern;
+
+public sealed class Singleton
+{
+    private static Singleton? _Instance { get; set; }
+
+
+    private Singleton()
+    {
+        
+    }
+
+    public static Singleton CreateInstance()
+    {
+        return _Instance ??= new Singleton(); // if many threads reach this line this could cause many instanciation
+                                 
+    }
+}
